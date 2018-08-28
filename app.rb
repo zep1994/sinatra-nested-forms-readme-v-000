7029e3a233d3222793817bde7ca345f62b37f88d
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-08-28 02:06:53 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,3 +1,13 @@
-class App < Sinatra::Base 
-  
+class App < Sinatra::Base
+   get '/' do
+    erb :new
+  end
+   post '/student' do
+    @student = Student.new(params[:student])
+     params[:student][:course].each do |details|
+      Course.new(details)
+  end
+   @course = Course.new
+   erb :student
 end
+ end
