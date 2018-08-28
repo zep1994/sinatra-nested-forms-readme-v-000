eacb9b37717b0520156ac8ede7429f5b7e4ffb5c
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-08-28 02:07:03 +0000

    Done.

--- model/student.rb
+++ model/student.rb
@@ -1,14 +1,12 @@
-<h1>Student</h1>
- 
-<div class="student">
-  <h3>Name: <%= @student.name %></h3><br>
-  <h4>Grade: <%= @student.grade %></h4>
-</div><br>
- 
-<h1>Courses</h1>
-<% @courses.each do |course| %>
-  <div class="course">
-    <p>Name: <%= course.name %></p><br>
-    <p>Topic: <%= course.topic %></p><br>
-  </div><br>
-<% end %> 
+class Student
+   attr_reader :name, :grade
+   STUDENTS = []
+   def initialize(params)
+    @name = params[:name]
+    @grade = params [:grade]
+      STUDENTS << selfß
+  end
+   def self.all
+    STUDENTS
+  end
+ end
